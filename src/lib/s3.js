3f791bac4f862755df2178ae609db4b264bafb30
Admin <dmadhusudhan98@gmail.com>
2026-05-09 00:24:03 +0530

    added

--- src/lib/s3.js
+++ src/lib/s3.js
@@ -2,9 +2,9 @@
 import AWS from 'aws-sdk';
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+  region: process.env.MY_AWS_REGION,
 });
 
 export default s3;
