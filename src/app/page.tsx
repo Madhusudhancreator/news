22a6b2bd6cc6c3aef8452d2d6035fb6288b78f3f
Admin <dmadhusudhan98@gmail.com>
2026-05-28 19:29:07 +0530

    fix: add force-dynamic to prevent static pre-rendering of Prisma pages at build time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import HomeLayout from "@/components/layouts/HomeLayout";
 import BodyContainer from "@/components/common/BodyContainer";
